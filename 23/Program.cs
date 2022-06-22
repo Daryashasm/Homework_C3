d30463f7d5d2da84c1150cc8007638062b14e343
Daria <darena-smol@yandex.ru>
2022-06-22 22:14:27 +0500

    3

---
+++ 23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число; N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Таблица кубов от 1 до {N}");
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"Куб числа {i} = {i*i*i}");
+}
