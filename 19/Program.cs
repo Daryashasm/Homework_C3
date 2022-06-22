a58a51637b53728f5d254ce0ef9af4ed9018f569
Daryashasm <105740225+Daryashasm@users.noreply.github.com>
2022-06-22 21:34:32 +0500

    Update Program.cs

--- 19/Program.cs
+++ 19/Program.cs
@@ -5,11 +5,11 @@ if (numberA > 9999 && numberA < 100000)
     string numberB = numberA.ToString();
     if (numberB[0] == numberB[4] && numberB[1] == numberB[3])
     {
-    Console.WriteLine($"{numberA} - это число является палидром");
+    Console.WriteLine($"{numberA} - это число является палиндром");
     }
     else
     {
-     Console.WriteLine($"{numberA} - это число не является палидром");
+     Console.WriteLine($"{numberA} - это число не является палиндром");
     }
 }
 else
